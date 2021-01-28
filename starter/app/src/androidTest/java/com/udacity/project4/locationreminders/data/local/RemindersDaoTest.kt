d05c2af97106a00ae3e6ebb288ccb1e94288a2e9
Joe Awwad <joe.awwad@getray.com>
2021-01-28 17:02:25 +0200

    Implements RemindersDaoTest

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -2,22 +2,19 @@ package com.udacity.project4.locationreminders.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.SmallTest;
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
-
-import kotlinx.coroutines.ExperimentalCoroutinesApi;
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -25,6 +22,68 @@ import org.junit.Test
 @SmallTest
 class RemindersDaoTest {
 
-//    TODO: Add testing implementation to the RemindersDao.kt
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var remindersDao: RemindersDao
+
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun setUp() {
+        database = Room.inMemoryDatabaseBuilder(
+            getApplicationContext(),
+            RemindersDatabase::class.java
+        ).build()
+
+        remindersDao = database.reminderDao()
+    }
+
+    @After
+    fun tearDown() = database.close()
+
+    @Test
+    fun shouldGetReminderById() = runBlockingTest {
+        val reminder = ReminderDTO("TITLE1", "DESCRIPTION1", "LOCATION1", 0.0, 0.0)
+
+        remindersDao.saveReminder(reminder)
+
+        val result = remindersDao.getReminderById(reminder.id)
+
+        assertThat(result as ReminderDTO, `is`(notNullValue()))
+        assertThat(result.id, `is`(reminder.id))
+        assertThat(result.title, `is`(reminder.title))
+        assertThat(result.description, `is`(reminder.description))
+        assertThat(result.location, `is`(reminder.location))
+    }
+
+    @Test
+    fun shouldSaveAndGetAllReminders() = runBlockingTest {
+        val reminder1 = ReminderDTO("TITLE1", "DESCRIPTION1", "LOCATION1", 0.0, 0.0)
+        val reminder2 = ReminderDTO("TITLE2", "DESCRIPTION2", "LOCATION2", 0.0, 0.0)
+
+        remindersDao.saveReminder(reminder1)
+        remindersDao.saveReminder(reminder2)
+
+        val result = remindersDao.getReminders()
+
+        assertThat(result.size, `is`(2))
+        assertThat(result, hasItem(reminder1))
+        assertThat(result, hasItem(reminder2))
+    }
+
+    @Test
+    fun shouldDeleteAllReminders() = runBlockingTest{
+        val reminder1 = ReminderDTO("TITLE1", "DESCRIPTION1", "LOCATION1", 0.0, 0.0)
+        val reminder2 = ReminderDTO("TITLE2", "DESCRIPTION2", "LOCATION2", 0.0, 0.0)
+
+        remindersDao.saveReminder(reminder1)
+        remindersDao.saveReminder(reminder2)
+
+        remindersDao.deleteAllReminders()
+
+        val reminders = remindersDao.getReminders()
+
+        assertThat(reminders, `is`(emptyList()))
+    }
 }
--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -3,26 +3,40 @@ package com.udacity.project4.locationreminders.data
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
+/**
+ * FakeDataSource: test double to the LocalDataSource
+ */
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    var reminders = LinkedHashMap<String, ReminderDTO>()
+
+    var shouldReturnError = false
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Could not get reminders")
+        }
+
+        return Result.Success(reminders.values.toList())
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders[reminder.id] = reminder
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError) {
+            return Result.Error("Could not get reminder $id")
+        }
+
+        reminders[id]?.let {
+            return Result.Success(it)
+        }
+
+        return Result.Error("Could not find reminder: $id")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
-
-
 }
